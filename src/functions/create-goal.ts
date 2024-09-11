b1d3a0487b850865daaf6868ada4d460a27419c2
LsPelegrina <lspelegrina@gmail.com>
2024-09-11 03:36:51 -0300

    Initial Commit

---
+++ src/functions/create-goal.ts
@@ -0,0 +1,10 @@
+import { db } from "../db";
+
+interface CreateGoalRequest {
+  title: string;
+  desiredWeeklyFrequency: number;
+}
+
+export async function createGoal({ title, desiredWeeklyFrequency } : CreateGoalRequest) {
+  await db.
+}
